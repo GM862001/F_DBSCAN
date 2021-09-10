2dee341e1e6113be8e29f444ca1eb1d6b532383f
GM862001 <88272671+GM862001@users.noreply.github.com>
2021-09-10 15:26:05 +0200

    Update fd_dbscan.py

--- VF_DBSCAN/fd_dbscan.py
+++ VF_DBSCAN/fd_dbscan.py
@@ -2,7 +2,6 @@ import numpy as np
 from typing import List, Dict
 from scipy.spatial import distance
 from numba import jit
-import time
 
 @jit(nopython=True)
 def numba_euclidean_distance(u:np.ndarray, v:np.ndarray):
@@ -28,13 +27,10 @@ class FDBSCAN_Client():
         points = np.array(self.__get_points())
         n_points = len(points)
         matrix = [[0] * n_points for i in range(n_points)]
-        start = time.time()
         for i in range(n_points):
             for j in range(n_points):
                 if (numba_euclidean_distance(points[i], points[j]) < epsilon):
                     matrix[i][j] = 1
-        end = time.time()
-        print(f'compute_neighborhood_matrix {end - start}')
         return matrix
 
     def update_labels(self, labels: List):
